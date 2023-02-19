df5d9b259e669bb4a22833659de14924fa005483
hellen <mumohellen9@gmail.com>
2023-02-19 14:24:16 -0800

    task6

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: A code that prints all the letters in lower case except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
