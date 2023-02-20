9cc6a5528020063891a3fe5ced2855e7ca4441ad
hellen <mumohellen9@gmail.com>
2023-02-19 17:41:15 -0800

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c program that prints a line to the standard erro
- * Return 1 (succes)
- */
+*main - A c program that prints a line to the standard erro
+*Return (1) (succes)
+*/
 int main(void)
 {
 	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19", stdout);
-	return (1);
+        return (1);
 }
