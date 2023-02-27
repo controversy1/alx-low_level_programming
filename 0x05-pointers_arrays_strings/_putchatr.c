a0a7c870eae168d9b1a1c42126da7a339924edd2
hellen <mumohellen9@gmail.com>
2023-02-27 08:19:03 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/_putchatr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character cto stot
+ * @c: the character tyo print
+ *  
+ *Return: 1 on succes
+ * On erro, -1 is returned
+ */
+int _putchar(char c)
+{
+	        return (write (1, &c, 2));
+}
