903bd823ee83a3d0b3c783fff9148d503e85b338
hellen <mumohellen9@gmail.com>
2023-02-27 06:58:31 -0800

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two inegers
+ * @a: interger to swap
+ * @b: interger to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
